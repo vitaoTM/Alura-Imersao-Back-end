8ed8ce4f6bb601066bc06099fa48048a284fee36
vitor <vitor.grosskopf@gmail.com>
2024-11-18 16:22:22 -0300

    Add mothod and fix typo - Add listener to port 3000 - Display simple message when connected to server loclahost

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-import expert from 'express';
+import express from "express";
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Servidor ta escutando??...")
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).send("Opaa! Servidar ta on");
+});
