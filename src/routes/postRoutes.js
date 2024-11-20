31b07a80f14e417bbb5c1cd22b416f09d4185a33
vitor <vitor.grosskopf@gmail.com>
2024-11-20 16:48:37 -0300

    Update routes file

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,13 +1,10 @@
 import express from "express";
+import { listPosts } from "../controllers/postsController.js";
 
 const routes = (app) => {
   app.use(express.json());
 
-  app.get("/posts", async (req, res) => {
-    const posts = await getAllPosts();
-    // const index = postSearchID(req.params.id)
-    res.status(200).json(posts);
-  });
+  app.get("/posts", listPosts);
 }
 
 export default routes;
