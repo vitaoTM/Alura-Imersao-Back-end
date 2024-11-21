794ce3a5545780a088ba45a7bc099b0f8aefa532
vitor <vitor.grosskopf@gmail.com>
2024-11-21 17:11:22 -0300

    Add multer for storage management - run: npm install multer

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -17,3 +17,14 @@ export async function postNewPost(req, res) {
         res.status(500).json({"Error":"Falha na requisição"});
     }
 }
+
+export async function imageUpload(req, res) {
+    const newPost = req.body;
+    try {
+        const createdPost = await createPost(newPost);
+        res.status(200).json(createdPost);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({"Error":"Falha na requisição"});
+    }
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { listPosts, postNewPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listPosts, postNewPost, imageUpload } from "../controllers/postsController.js";
+
+const upload = multer({dest:"./uploads", storage})
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listPosts);
-    app.post("/posts", postNewPost)
+    app.post("/posts", postNewPost);
+    app.post("/upload", upload.single("img"), imageUpload);
 }
 
 export default routes;
