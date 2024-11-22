35e5e0ffdb1dabcb69bbc57d8d76bd360e5af08e
vitor <vitor.grosskopf@gmail.com>
2024-11-22 16:06:21 -0300

    Fix mistake on posts update - Bug on updatePosts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,13 +1,13 @@
-import { error } from "console";
 import { getAllPosts, createPost, updatePost } from "../models/postsModels.js";
 import fs from "fs";
+
 
 export async function listPosts (req, res)
 {
     const posts =  await getAllPosts();
+
     res.status(200).json(posts);
 }
-
 
 export async function postNewPost(req, res) {
     const newPost = req.body;
@@ -26,6 +26,7 @@ export async function imageUpload(req, res) {
         imgUrl: req.file.originalname,
         alt: '',
     };
+
     try {
         const createdPost = await createPost(newPost);
         const updatedImg = `uploads/${createdPost.insertedId}.png`;
@@ -40,14 +41,15 @@ export async function imageUpload(req, res) {
 export async function updateNewPost(req, res) {
     const id = req.params.id;
     const urlImg = `http://localhost:3000/${id}.png`
+
     const post = {
         imgUrl: urlImg,
         description: req.body.description,
         alt: req.body.alt
     }
     try {
-        const updatePost = await updatePost(id, post);
-        res.status(200).json(updatePost);
+        const createdPost = await updatePost(id, post);
+        res.status(200).json(createdPost);
     } catch(error) {
         console.error(error.message);
         res.status(500).json({"Error":"Falha na requisição"})
