7239da37220a3e57a9f526b3bbde5ee3c1d13aab
vitor <vitor.grosskopf@gmail.com>
2024-11-22 15:27:49 -0300

    Adds route to update posts - adds PUT verb - Clen up code a bit

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { listPosts, postNewPost, imageUpload } from "../controllers/postsController.js";
+import { listPosts, postNewPost, imageUpload, updateNewPost } from "../controllers/postsController.js";
 
 const upload = multer({dest: "./uploads"})
 
@@ -9,6 +9,8 @@ const routes = (app) => {
     app.get("/posts", listPosts);
     app.post("/posts", postNewPost);
     app.post("/upload", upload.single("img"), imageUpload);
+
+    app.put("/upload/:id", updateNewPost)
 }
 
 export default routes;
