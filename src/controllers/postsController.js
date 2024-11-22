4ed5b705df1609c4086691bd977daca73c2c174b
vitor <vitor.grosskopf@gmail.com>
2024-11-22 15:22:33 -0300

    Adds async function in controller - function to update

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
-import { getAllPosts, createPost } from "../models/postsModels.js";
+import { error } from "console";
+import { getAllPosts, createPost, updatePost } from "../models/postsModels.js";
 import fs from "fs";
 
 export async function listPosts (req, res)
@@ -27,11 +28,28 @@ export async function imageUpload(req, res) {
     };
     try {
         const createdPost = await createPost(newPost);
-        const updatedImg = `uploads/${createPost.insertedId}.png`;
+        const updatedImg = `uploads/${createdPost.insertedId}.png`;
         fs.renameSync(req.file.path, updatedImg);
         res.status(200).json(createdPost);
     } catch (error) {
         console.error(error.message);
         res.status(500).json({"Error":"Falha na requisição"});
+    }
+}
+
+export async function updateNewPost(req, res) {
+    const id = req.params.id;
+    const urlImg = `http://localhost:3000/${id}.png`
+    const post = {
+        imgUrl: urlImg,
+        description: req.body.description,
+        alt: req.body.alt
+    }
+    try {
+        const updatePost = await updatePost(id, post);
+        res.status(200).json(updatePost);
+    } catch(error) {
+        console.error(error.message);
+        res.status(500).json({"Error":"Falha na requisição"})
     }
 }
