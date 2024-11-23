be25df933695576c9d4f0f6b49cb9643551be6d8
vitor <vitor.grosskopf@gmail.com>
2024-11-23 17:16:13 -0300

    Add in routes CORS for cross-origin reques

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,16 +1,24 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { listPosts, postNewPost, imageUpload, updateNewPost } from "../controllers/postsController.js";
+
+const corsOption = {
+    origin: "http://localhost:8000",
+    optionssuccsessStatus: 200,
+};
 
 const upload = multer({dest: "./uploads"})
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOption));
     app.get("/posts", listPosts);
     app.post("/posts", postNewPost);
     app.post("/upload", upload.single("img"), imageUpload);
 
     app.put("/upload/:id", updateNewPost)
+
 }
 
 export default routes;
